1ef35dac16b8ab16a5de017e5c3d6dba4162550c
Erika Fomich <erikafomich@gmail.com>
2021-08-20 10:41:51 -0700

    added flashing color and computer choice

--- docs/app.js
+++ docs/app.js
@@ -1,1 +1,18 @@
-console.log('connected');
+const randomNum = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min +1) + min);
+}
+
+let computerChoice = [];
+computerChoice.push(randomNum(1,4));
+computerChoice.push(randomNum(1,4));
+computerChoice.push(randomNum(1,4));
+console.log(computerChoice);
+
+
+/* source:
+random number -
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+*/
