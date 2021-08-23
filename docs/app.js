89e98e4c72fec8172f43c19ef553a4ac30759436
Erika Fomich <erikafomich@gmail.com>
2021-08-22 18:28:56 -0700

    added alert to check if user pattern was correct

--- docs/app.js
+++ docs/app.js
@@ -1,4 +1,5 @@
-let computerChoices = [];
+const computerChoices = [];
+const userChoices = [];
 const red = document.querySelector("#one");
 const blue = document.querySelector("#two");
 const green = document.querySelector("#three");
@@ -60,7 +61,26 @@ computerChoices.forEach((num, i) => {
 const userSelect = (box) => {
     box.addEventListener('click', () => {
         box.classList.add ("flash");
+        if(red.classList.contains("flash")){
+            userChoices.push(1);
+            console.log(userChoices);
+            checkChoices();
+        } else if(blue.classList.contains("flash")){
+            userChoices.push(2);
+            console.log(userChoices);
+            checkChoices();
+        } else if(green.classList.contains("flash")){
+            userChoices.push(3);
+            console.log(userChoices);
+            checkChoices();
+        } else if(yellow.classList.contains("flash")){
+            userChoices.push(4);
+            console.log(userChoices);
+            checkChoices();
+        }
         setTimeout(removeBoxFlash, 500, box);
+        
+
     })
 }
 
@@ -68,6 +88,18 @@ userSelect(red);
 userSelect(blue);
 userSelect(green);
 userSelect(yellow);
+
+const checkChoices = () =>{
+    if(userChoices.length === computerChoices.length){
+    for (let i = 0; i< computerChoices.length; i++){
+        if(userChoices[i] === computerChoices[i]){
+            alert('That was right');
+        }else{
+            alert('That was not right');
+        }
+    }
+    }
+}
 
 /* sources:
 random number -
